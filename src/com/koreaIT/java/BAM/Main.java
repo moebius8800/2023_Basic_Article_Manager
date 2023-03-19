4e03257b2dfef6317044de3c28814adb49e23870
moebius8800 <moebius8800@gmail.com>
2023-03-19 17:24:50 +0900

    게시판 기능 구현중

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -1,5 +1,7 @@
 package com.koreaIT.java.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,10 +12,12 @@ public class Main {
 
 		int lastArticleId = 0;
 
+		List<Article> articles = new ArrayList<>();
+		
 		while (true) {
 
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
@@ -30,6 +34,8 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
+				Article article = new Article(id, title, body);
+
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
 
 			} else if (cmd.equals("article list")) {
@@ -44,5 +50,17 @@ public class Main {
 
 		sc.close();
 
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
