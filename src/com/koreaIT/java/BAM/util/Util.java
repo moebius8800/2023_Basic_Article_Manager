d49d510b8fb3950709f1eafd10ba4124dd16d868
moebius8800 <moebius8800@gmail.com>
2023-03-20 02:06:01 +0900

    리팩토링. 패키지 분리, 핵심 로직 이전

--- src/com/koreaIT/java/BAM/util/Util.java
+++ src/com/koreaIT/java/BAM/util/Util.java
@@ -1,13 +1,13 @@
-package com.koreaIT.java.BAM;
+package com.koreaIT.java.BAM.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Util {
 	public static String getDate() {
-
+		
 		Date date = new Date();
-
+		
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss");
 
 		return formatter.format(date);
